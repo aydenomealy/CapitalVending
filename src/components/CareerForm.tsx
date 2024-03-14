a8a9cb33ca272def88d74db07417644e4895da5a
Ayden <aydenomealy@gmail.com>
2024-03-14 05:09:33 -0700

    Added photos, started career form

---
+++ src/components/CareerForm.tsx
@@ -0,0 +1,10 @@
+
+const CareerForm = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CareerForm;
--- src/config/index.ts
+++ src/config/index.ts
@@ -2,11 +2,11 @@ import {z} from "zod";
 
 export const PRODUCT_CATEGORIES = [
     {
-        label: "Coin-operated Games",
+        label: "Arcade and Amusements",
         value: "coin_operated_games" as const,
         featured: [
             {
-                name: "Pinballs",
+                name: "Arcade Machines",
                 href: "#",
                 imageSrc: "/nav/machines/pinball.jpg"
             },
@@ -45,44 +45,49 @@ export const PRODUCT_CATEGORIES = [
         ]
     },
     {
-        label: "Bulk Vending and Jukeboxes",
-        value: "bulk_vending_jukeboxes" as const,
+        label: "Traditional Amusements",
+        value: "traditional_amusements" as const,
         featured: [
             {
                 name: "Touch Tunes Jukebox",
                 href: "#",
-                imageSrc: "/nav/merchandise/pinball.jpg"
-            },
-            {
-                name: "Traditional CD Jukeboxes",
-                href: "#",
                 imageSrc: "/nav/merchandise/jukebox.jpg"
             },
             {
-                name: "Premium Vending Products",
+                name: "Dart Boards",
                 href: "#",
-                imageSrc: "/nav/merchandise/atm.jpg"
+                imageSrc: "/nav/merchandise/dart.jpg"
+            },
+            {
+                name: "Pool Tables",
+                href: "#",
+                imageSrc: "/nav/merchandise/pool.jpg"
+            },
+            {
+                name: "Pinball",
+                href: "#",
+                imageSrc: "/nav/merchandise/pinball.jpg"
             },
         ]
     },
     {
-        label: "Party & Event Rentals",
-        value: "party_event_rentals" as const,
+        label: "ATMs",
+        value: "atms" as const,
         featured: [
             {
-                name: "Touch Tunes Jukebox",
-                href: "#",
-                imageSrc: "/nav/merchandise/pinball.jpg"
-            },
-            {
-                name: "Traditional CD Jukeboxes",
-                href: "#",
-                imageSrc: "/nav/merchandise/jukebox.jpg"
-            },
-            {
-                name: "Premium Vending Products",
+                name: "ATM Service",
                 href: "#",
                 imageSrc: "/nav/merchandise/atm.jpg"
+            },
+            {
+                name: "Merchant Loading",
+                href: "#",
+                imageSrc: "/nav/merchandise/amt2.jpg"
+            },
+            {
+                name: "Event ATM's",
+                href: "#",
+                imageSrc: "/nav/merchandise/eventatm.jpg"
             },
         ]
     }
@@ -97,4 +102,26 @@ export const CONTACT_SCHEMA = z.object({
     }),
     phone: z.string(),
     message: z.string()
+})
+
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+
+export const CAREER_SCHEMA = z.object({
+    name: z.string().min(1, {
+        message: "Please enter your name"
+    }),
+    email: z.string().email({
+        message: "Please enter a valid email address"
+    }),
+    phone: z.string().min(7, {
+        message: "Please enter a valid phone number"
+    }),
+    resume: z
+        .any()
+        .refine((files) => files?.length == 1, "File is required.")
+        .refine((files) => files?.[0]?.size <= 700000)
+        .refine(
+            (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+            ".jpg, .jpeg, .png and .webp files are accepted."
+        ),
 })
